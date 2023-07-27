e76806b3157067e8ac05c23c5139a03e52faeb59
mdrokz <mohammadmunshi@gmail.com>
2023-07-27 20:41:01 +0530

    feat: add chat handlers & db pool

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-#[macro_use] extern crate rocket;
+use std::env;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+// use diesel::{Connection, PgConnection};
+use dotenvy::dotenv;
+
+use rocket_sync_db_pools::{diesel::PgConnection, database};
+
+#[macro_use]
+extern crate rocket;
+
+pub mod handlers;
+pub mod models;
+pub mod schema;
+
+use handlers::chat::{create_chat, delete_chat, get_chat, list_chats, update_chat};
+
+#[database("llama_chat")]
+pub struct DbConn(PgConnection);
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount(
+        "/chats",
+        routes![create_chat, delete_chat, get_chat, list_chats, update_chat],
+    ).attach(DbConn::fairing())
 }
