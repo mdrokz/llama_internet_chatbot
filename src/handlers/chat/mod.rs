503711535a0ac950a6e0c490cb403aab8b30c434
mdrokz <mohammadmunshi@gmail.com>
2023-07-27 20:40:50 +0530

    feat: move to async and use db pool request guard

--- src/handlers/chat/mod.rs
+++ src/handlers/chat/mod.rs
@@ -1,4 +1,4 @@
-use crate::{establish_connection_pg, models::Chat};
+use crate::{models::Chat, DbConn};
 use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl};
 use rocket::{
     response::{status::Created, Debug},
@@ -12,77 +12,85 @@ mod db;
 type Result<T, E = Debug<diesel::result::Error>> = std::result::Result<T, E>;
 
 #[post("/create", format = "json", data = "<chat>")]
-pub fn create_chat(chat: Json<NewChat>) -> Result<Created<Json<NewChat>>> {
+pub async fn create_chat(conn: DbConn, chat: Json<NewChat>) -> Result<Created<Json<NewChat>>> {
     use crate::schema::chats::dsl::*;
 
     use crate::models::Chat;
-
-    let connection = &mut establish_connection_pg();
 
     let new_chat = Chat {
         name: chat.name.clone(),
         ..Default::default()
     };
 
-    diesel::insert_into(chats)
-        .values(&new_chat)
-        .execute(connection)
-        .expect("Error saving new post");
+    conn.run(move |c| {
+        diesel::insert_into(chats)
+            .values(&new_chat)
+            .execute(c)
+            .expect("Error saving new post")
+    })
+    .await;
 
     Ok(Created::new("/").body(chat))
 }
 
 #[get("/", format = "json")]
-pub fn list_chats() -> Result<Json<Vec<Chat>>> {
+pub async fn list_chats(conn: DbConn) -> Result<Json<Vec<Chat>>> {
     use crate::schema::chats::dsl::*;
 
-    let connection = &mut establish_connection_pg();
-
-    let chats_list = chats
-        .limit(20)
-        .load::<Chat>(connection)
-        .expect("Error loading chats");
+    let chats_list = conn
+        .run(|c| {
+            chats
+                .limit(20)
+                .load::<Chat>(c)
+                .expect("Error loading chats")
+        })
+        .await;
 
     Ok(Json(chats_list))
 }
 
 #[get("/<id>", format = "json")]
-pub fn get_chat(id: uuid::Uuid) -> Result<Json<Chat>> {
+pub async fn get_chat(conn: DbConn, id: uuid::Uuid) -> Result<Json<Chat>> {
     use crate::schema::chats::dsl::chats;
 
-    let connection = &mut establish_connection_pg();
-
-    let chat = chats
-        .find(id)
-        .first::<Chat>(connection)
-        .expect("Error loading chat");
+    let chat = conn
+        .run(move |c| chats.find(id).first::<Chat>(c).expect("Error loading chat"))
+        .await;
 
     Ok(Json(chat))
 }
 
 #[put("/update/<id>", format = "json", data = "<chat>")]
-pub fn update_chat(id: uuid::Uuid, chat: Json<UpdateChat>) -> Result<Json<Chat>> {
+pub async fn update_chat(
+    conn: DbConn,
+    id: uuid::Uuid,
+    chat: Json<UpdateChat>,
+) -> Result<Json<Chat>> {
     use crate::schema::chats::dsl::{chats, name};
 
-    let connection = &mut establish_connection_pg();
-
-    let chat = diesel::update(chats.find(id))
-        .set(name.eq(chat.name.clone()))
-        .get_result::<Chat>(connection)
-        .expect("Error updating chat");
+    let chat = conn
+        .run(move |c| {
+            diesel::update(chats.find(id))
+                .set(name.eq(chat.name.clone()))
+                .get_result::<Chat>(c)
+                .expect("Error updating chat")
+        })
+        .await;
 
     Ok(Json(chat))
 }
 
 #[delete("/delete/<id>", format = "json")]
-pub fn delete_chat(id: uuid::Uuid) -> Result<Json<Chat>> {
+pub async fn delete_chat(conn: DbConn, id: uuid::Uuid) -> Result<Json<Chat>> {
     use crate::schema::chats::dsl::chats;
 
-    let connection = &mut establish_connection_pg();
-
-    let chat = diesel::delete(chats.find(id))
-        .get_result::<Chat>(connection)
-        .expect("Error deleting chat");
+    let chat = conn
+        .run(move |c| {
+            diesel::delete(chats.find(id))
+                .get_result::<Chat>(c)
+                .expect("Error deleting chat")
+        })
+        .await;
 
     Ok(Json(chat))
 }
