fedfff8239c85d029973a47bc7bce47a7e21c153
mdrokz <mohammadmunshi@gmail.com>
2023-08-06 01:31:59 +0530

    feat: setup fern logger

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ extern crate rocket;
 pub mod handlers;
 pub mod models;
 pub mod schema;
+pub mod utils;
+
+use fern::Dispatch;
+use log::LevelFilter;
+use rocket::serde::json::serde_json::json;
 
 use handlers::{
     chat::{create_chat, delete_chat, get_chat, list_chats, update_chat},
@@ -15,8 +20,36 @@ use handlers::{
 #[database("llama_chat")]
 pub struct DbConn(PgConnection);
 
+fn setup_logger() -> Result<(), fern::InitError> {
+    let file = fern::log_file("logs/output.log")?;
+    Dispatch::new()
+        .format(|out, message, record| {
+            let log = json!({
+                "timestamp": chrono::Local::now().to_rfc3339(),
+                "level": record.level().to_string(),
+                "message": message.to_string(),
+            });
+
+            out.finish(format_args!("{}", log))
+            // out.finish(format_args!(
+            //     "{} [{}] {}",
+            //     chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+            //     format!("{}", record.level()).color(color),
+            //     message
+            // ))
+        })
+        .level(LevelFilter::Info)
+        .chain(std::io::stdout())
+        .chain(file)
+        .apply()?;
+    Ok(())
+}
+
 #[launch]
 fn rocket() -> _ {
+
+    let _ = setup_logger();
+
     rocket::build()
         .mount(
             "/chats",
